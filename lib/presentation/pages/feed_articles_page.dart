4da7053438dcbd6528cd87ef17bd816a8ac141c0
Ratchapong Prasertchartmora <ratchapong.p@umt.ltd>
2022-12-05 20:33:59 +0700

    feat: add feed articles page and provider

---
+++ lib/presentation/pages/feed_articles_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FeedArticlesPage extends StatelessWidget {
+  const FeedArticlesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/providers/feed_articles_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class FeedArticlesProvider with ChangeNotifier {
+
+}
